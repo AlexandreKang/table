9d5db44551063884a476d70c387c5faf086ca7f4
Alexandre <amk04@live.ca>
2018-08-23 17:15:04 -0400

    Update

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { MatFormFieldModule, MatTableModule, MatInputModule } from '@angular/material';
+import { MatFormFieldModule, MatTableModule, MatInputModule, MatToolbarModule, MatButtonModule, MatPaginatorModule, MatSortModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
-import { MatPaginatorModule } from '@angular/material/paginator';
 import { FormsModule } from '@angular/forms';
-import { MatSortModule } from '@angular/material/sort';
 
 import { AppComponent } from './app.component';
 import { TableOverviewExampleComponent } from './table-overview-example/table-overview-example.component';
-
+import { ProgressSpinnerConfigurableExampleComponent } from './progress-spinner-configurable-example/progress-spinner-configurable-example.component';
 @NgModule({
   declarations: [
     AppComponent,
     TableOverviewExampleComponent,
+    ProgressSpinnerConfigurableExampleComponent
   ],
 
   imports: [
@@ -23,7 +22,9 @@ import { TableOverviewExampleComponent } from './table-overview-example/table-ov
     BrowserAnimationsModule,
     MatPaginatorModule,
     FormsModule,
-    MatSortModule
+    MatSortModule,
+    MatToolbarModule, 
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent],
